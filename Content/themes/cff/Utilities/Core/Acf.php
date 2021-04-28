d5e668274481ae21ee5e825e3fa593e0ef91000c
Alex Woollam <alex.woollam@proagrica.com>
2021-04-28 12:13:23 +0100

    set admin freakout if acf isnt installed.

--- Content/themes/cff/Utilities/Core/Acf.php
+++ Content/themes/cff/Utilities/Core/Acf.php
@@ -9,7 +9,23 @@ namespace CFF\Utilities\Core;
 class Acf{
 
     public function __construct(){
+
+        if( ! class_exists('ACF')  )
+        {
+            add_action( 'admin_notices', function(){
+                ?>
+                <div class="notice notice-error is-dismissible">
+                    <p><?php _e( 'ACF NOT FOUND! - check composer is updated and installed on this server.', 'cff' ); ?></p>
+                </div>
+                <?php
+            } );
+            return;
+        }
                 
+        
+    }
+
+    public function set_save_location(){
         add_filter('acf/settings/save_json', function($path){
             $path = THEME_CORE . '/acf/json';
             return $path;
